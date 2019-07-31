be9727f7236044e596484dec7996b0993b3fd246
AhmadAbulimet <1211ahmad@gmail.com>
2019-07-31 16:36:34 -0400

    my some modification

--- firstTest.java
+++ firstTest.java
@@ -3,5 +3,6 @@ package tests;
 public class firstTest {
     public static void main(String[] args) {
         System.out.println("Hell World");
+        System.out.println("i love selenium");
     }
 }
